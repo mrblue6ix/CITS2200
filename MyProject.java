8b69851d64502740796b08647a2b5e4beccadf68
mrblue6ix <Jordan-moss1@hotmail.com>
2021-05-17 12:54:55 +0800

    added project files

---
+++ MyProject.java
@@ -0,0 +1,34 @@
+// Jordan Mossammaparast (22774949)
+
+public class MyProject implements Project {
+	
+	public MyProject() {
+		//TODO constructor
+	}
+	
+    public boolean allDevicesConnected(int[][] adjlist) {
+        // TODO
+        return false;
+    }
+
+    public int numPaths(int[][] adjlist, int src, int dst) {
+        // TODO
+        return 0;
+    }
+
+    public int[] closestInSubnet(int[][] adjlist, short[][] addrs, int src, short[][] queries) {
+        // TODO
+        return null;
+    }
+
+    public int maxDownloadSpeed(int[][] adjlist, int[][] speeds, int src, int dst) {
+        // TODO
+        return 0;
+    }
+    
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
